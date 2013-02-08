526dccbab82be47d1445a73dbe6933ac19eae549
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-02-08 10:05:35 -0500

    first commit

---
+++ friends.sql
@@ -0,0 +1,10 @@
+create table friends
+(
+  id serial4 primary key,
+  name varchar(100),
+  age varchar(10),
+  gender varchar(500),
+  image varchar(500),
+  twitter varchar(505),
+  github varchar(505)
+);
